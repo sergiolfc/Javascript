93bd7f8031b515a818a22464afb1e51d36f1792e
sergio.burocco <sergioburocco@gmail.com>
2022-07-21 21:30:13 -0300

    primerEntrega

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-const notasAlumno = [9, 6, 3, 9];
-let sumadorNotas = 0;
-for(let posicion =0; posicion <4; posicion++) {
-    sumadorNotas = sumadorNotas + notasAlumno[posicion];
-}
-const promedio = sumadorNotas /4;
-console.log(promedio); 
+const carrito = [];
 
-if(promedio > 6) {
-    console.log("Aprobaste")
-} 
-else { 
-    console.log("No aprobaste, para la proxima estudia mas") 
+function agregarAlCarrito(producto){
+    carrito.push(producto);
+    console.log(producto);
 }
+
+agregarAlCarrito({id: 1, type: 'llavero', name: 'Metallica', precio: 150})
+agregarAlCarrito({id: 2, type: 'llavero', name: 'Maiden', precio: 100})
+agregarAlCarrito({id: 3, type: 'llavero', name: 'Ac/dc', precio: 100})
+agregarAlCarrito({id: 4, type: 'llavero', name: 'Motorhead', precio: 100})
+agregarAlCarrito({id: 5, type: 'llavero', name: 'Blondie', precio: 100})
+
+function borrarProductoDelCarrito(idDelProducto){
+    const index = carrito.findIndex((producto) => producto.id === idDelProducto);
+
+    if(index !== -1) {
+        carrito.splice(index, 1);
+}
+console.log(carrito); 
+}
+
+borrarProductoDelCarrito(5)
